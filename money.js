4f5d3546e7229f06331b417d730a7166da558443
bvalentic <bobvalentic@quickenloans.com>
2021-11-24 18:20:42 -0500

    Add comments and todos.

--- money.js
+++ money.js
@@ -1,16 +1,10 @@
+
+
 const buildingList = {
   TEMPLE: {
     name: "Temple",
     cost: 100,
     output: 10
-  }
-}
-
-class Building {
-  constructor(buildingName) {
-    this.name = buildingName.name
-    this.cost = buildingName.cost
-    this.output = buildingName.output
   }
 }
 
@@ -24,6 +18,27 @@ const jobList = {
     title: "Priest",
     description: "A member of the clergy.",
     wage: 5
+  },
+  //
+}
+
+const eraList = {
+  NEOLITHIC = {}, //Stone Age
+  BRONZE_AGE = {},
+  IRON_AGE = {},
+  ANTIQUITY = {},
+  MEDIEVAL = {}, //Middle Ages
+  EARLY_MODERN = {}, //Renaissance
+  LATE_MODERN = {}, //Industrialization
+  DIGITAL = {}, //Contemporary
+  FUTURE = {} //??? 
+}
+
+class Building {
+  constructor(buildingName) {
+    this.name = buildingName.name
+    this.cost = buildingName.cost
+    this.output = buildingName.output
   }
 }
 
@@ -46,10 +61,33 @@ class Person {
   }
 }
 
-var temple = new Building(buildingList.TEMPLE)
-var priest = new Person()
-priest.setJob(jobList.PRIEST)
+function main() {
+  var temple = new Building(buildingList.TEMPLE)
+  var priest = new Person()
+  priest.setJob(jobList.PRIEST)
 
-console.log(temple)
-console.log(priest)
-console.log(priest.job.title)
+  console.log(temple)
+  console.log(priest)
+  console.log(priest.job.title)
+}
+
+main();
+
+/* TODOS:
+
+- make UI and clickable buttons, output and global stats for each
+
+- need a way to regulate income in form of money per second, while having a continuous stream of income 
+(ex. if user gets $1000/s it is a continuous increase of 1, 2, 3, ... all the way to 1000 in the course of one second)
+
+- create list of buildings/people
+
+- there is a net pool of people which slowly grows/shrinks depending on global prosperity, 
+and the # of people which can be in your employ is a function of both the # of buildings owned and the total population
+
+- need various upgrades/powers to grow as the # of buildings/people/$ all grows or certain criteria are met
+
+- 
+
+*/
+
